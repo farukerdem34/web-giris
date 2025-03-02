7484ce756cffc7e475e5646b5f21c5c1795b2f7a
Faruk <farukomererdem2003@gmail.com>
2025-03-02 13:11:53 +0300

    cors

--- core/src/main.rs
+++ core/src/main.rs
@@ -1,6 +1,7 @@
 use crate::handlers::AppState;
 use crate::routes::config;
-use actix_web::{web, App, HttpServer};
+use actix_cors::Cors;
+use actix_web::{middleware, web, App, HttpServer};
 use sqlx::PgPool;
 use std::env;
 
@@ -18,8 +19,21 @@ async fn main() -> std::io::Result<()> {
 
     let app_state = web::Data::new(AppState { pool });
     println!("Starting server at http://{}:{}", HOST, PORT);
-    HttpServer::new(move || App::new().app_data(app_state.clone()).configure(config))
-        .bind(format!("{}:{}", HOST, PORT))?
-        .run()
-        .await
+    HttpServer::new(move || {
+        let cors = Cors::default()
+            .allow_any_origin() // Tüm kaynaklara izin ver (*)
+            .allow_any_method() // Tüm HTTP metodlarına izin ver (GET, POST vs.)
+            .allow_any_header() // Tüm başlıklara izin ver
+            .supports_credentials() // İsteğe bağlı: Kimlik bilgilerini destekle
+            .max_age(3600); // Preflight isteklerinin önbelleğe alınma süresi (saniye)
+
+        App::new()
+            .wrap(cors)
+            .wrap(middleware::Logger::default())
+            .app_data(app_state.clone())
+            .configure(config)
+    })
+    .bind(format!("{}:{}", HOST, PORT))?
+    .run()
+    .await
 }
